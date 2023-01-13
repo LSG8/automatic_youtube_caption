8d593944fb638f515bfd9ef735ecff2bf1a9ae33
Sengupta <senguptal@ap.teo.earth>
2023-01-13 18:08:25 +0530

    basic transcript and translate

--- helper/helper_config.py
+++ helper/helper_config.py
@@ -1,7 +1,8 @@
+import sys
 import configparser
 
 # Method to read config file settings
 def read_config():
     config = configparser.ConfigParser()
-    config.read('configurations.ini')
+    config.read('../configurations.ini')
     return config
--- src/main.py
+++ src/main.py
@@ -4,24 +4,26 @@ import speech_recognition as sr
 from os import path
 from googletrans import Translator
 
-sys.path.append('../helper')
+sys.path.append('../')
 sys.path.append('../audio_examples')
 sys.path.append('../output')
+
 from helper import helper_config
-#from helper import helper_config as input_settings
-#import helper.helper_config as input_settings
 
 r = sr.Recognizer()
+config = helper_config.read_config()
+settings = config['SETTINGS']
 
 def read_audio(file):
-    with file as source:
+    audio_file = sr.AudioFile(file)
+    with audio_file as source:
         r.adjust_for_ambient_noise(source, duration=0.1)
-        audio = r.record(source)  # read the entire audio file
+        audio = r.record(source)
         return audio
 
 def get_transcript(audio):
     try:
-        text = r.recognize_google(audio, language = "bn")
+        text = r.recognize_google(audio, language = settings['source_language'])
         return text
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
@@ -32,16 +34,19 @@ def get_transcript(audio):
 
 def translate_text(text):
     translator = Translator()
-    out = translator.translate(text, dest='en')
+    out = translator.translate(text, dest=settings['dest_language'])
     return out
+
+def write_to_file(file, text, mode):
+    f = open(file, mode, encoding='utf-8')
+    f.write(text)
+    f.close()
 
 
 
 if __name__ == '__main__':
-    print("Hello")
-    config = helper_config.read_config()
-    settings = config['SETTINGS']
-    print(settings['input_audio'])
-    print(settings['output_transcript'])
-    print(settings['source_language'])
-    print(settings['dest_language'])
+    audio = read_audio(settings['input_audio'])
+    transcript = get_transcript(audio)
+    translation = translate_text(transcript)
+    print(translation.text)
+    write_to_file(settings['output_transcript'],translation.text, "w+")
