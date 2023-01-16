0f4da22c13ff31ff1095f18c8c43b0064369a4bd
Sengupta <senguptal@ap.teo.earth>
2023-01-16 19:32:49 +0530

    split and translate

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import sys
 import speech_recognition as sr
 from os import path
 from googletrans import Translator
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 sys.path.append('../')
 sys.path.append('../audio_examples')
@@ -19,7 +21,20 @@ def read_audio(file):
     with audio_file as source:
         r.adjust_for_ambient_noise(source, duration=0.1)
         audio = r.record(source)
+        print(source.DURATION)
         return audio
+
+def split_audio_with_silence(file):
+    audio = AudioSegment.from_file(file, "wav") #change hardcoding
+    audio_chunks = split_on_silence(audio, min_silence_len=15, silence_thresh=-43 ) #change hardcoding
+    """ This function takes sound as a parameter which is our audio file next it takes min_silence_len by default it is 1000. 
+    The minimum length for silent sections is in milliseconds. 
+    If it is greater than the length of the audio segment an empty list will be returned. 
+    Here we are giving it as 500 milliseconds and silence_thresh by default it is -16. 
+    It is the upper bound for how quiet is silent in dBFS. We are giving it as -40. 
+    This function returns a list of audio segments. """
+    return list(filter(lambda x : len(x) > 1000, audio_chunks))
+
 
 def get_transcript(audio):
     try:
@@ -32,10 +47,30 @@ def get_transcript(audio):
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
         return ""
 
+""" def get_text(response):
+    for result in response.result2:
+        best_alternative = result.alternatives[0]
+        transcript = best_alternative.transcript
+        confidence = best_alternative.confidence
+        for word_info in best_alternative.words:
+            print(word_info.word)
+            print(word_info.start_time)
+            print(word_info.end_time)
+        print("-" * 80)
+        print(f"Transcript: {transcript}")
+        print(f"Confidence: {confidence:.0%}") """
+
 def translate_text(text):
     translator = Translator()
-    out = translator.translate(text, dest=settings['dest_language'])
-    return out
+    try:
+        out = translator.translate(text, dest=settings['dest_language'])
+        return out.text
+    except TypeError:
+        print("Type error, check the input")
+        return " "
+    except translator.raise_exception:
+        print("Could not translate this time")
+        return " "
 
 def write_to_file(file, text, mode):
     f = open(file, mode, encoding='utf-8')
@@ -45,8 +80,14 @@ def write_to_file(file, text, mode):
 
 
 if __name__ == '__main__':
-    audio = read_audio(settings['input_audio'])
-    transcript = get_transcript(audio)
-    translation = translate_text(transcript)
-    print(translation.text)
-    write_to_file(settings['output_transcript'],translation.text, "w+")
+    chunks = split_audio_with_silence(settings['input_audio'])
+    for audio_split in chunks:
+        print(len(audio_split))
+        output_file = "../output/chunk.wav" #change hardcoding
+        audio_split.export(output_file, format="wav")
+        audio = read_audio(output_file)
+        transcript = get_transcript(audio)
+        translation = translate_text(transcript)
+        print(translation)
+        print("-" * 80)
+    #write_to_file(settings['output_transcript'],translation.text, "w+")
